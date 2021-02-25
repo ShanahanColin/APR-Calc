db36b9918070eeff532a8e574456201029314ece
Colin Shanny <cshannyy@gmail.com>
2021-02-25 12:41:07 -0600

    working

--- Coding-Test/Bam.cs
+++ Coding-Test/Bam.cs
@@ -1,10 +1,32 @@
-﻿using System;
-namespace Coding_Test
+﻿namespace Coding_Test
 {
-    public class Calc
+     class Bam
     {
-        public Calc()
+        public static int CreditLimit { get; set; }
+        public static int AdvanceAmount { get; set; }
+        public static int Amount { get; set; }
+        public static int APR { get; set; }
+
+        public Bam(int creditLimit, int advanceAmount, int amount, int apr)
         {
+            CreditLimit = creditLimit;
+            AdvanceAmount = advanceAmount;
+            Amount = amount;
+            APR = apr;
+        }
+
+        //FIRST AMOUNT CALCULATION
+
+         public decimal EquationForOutput()
+        {
+            Amount = ((int)(CreditLimit * .5));
+            if (AdvanceAmount <= CreditLimit * .5)
+            {
+                Amount = AdvanceAmount;
+                return Amount;
+               
+            }
+            return Amount;
         }
     }
 }
