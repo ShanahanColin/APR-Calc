9278b91c5ee4fb9ac640dbfc358614f938ee9429
Colin Shanny <cshannyy@gmail.com>
2021-02-25 12:33:17 -0600

    first commit

---
+++ Coding-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coding_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
